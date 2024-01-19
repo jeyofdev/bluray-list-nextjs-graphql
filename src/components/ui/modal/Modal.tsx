44920f8ddd7ad36f9a0bd41a974586084fdf27e7
jeyofdev <jgregoire.pro@gmail.com>
2024-01-19 17:53:47 +0100

    feat : add search on modal

--- src/components/ui/modal/Modal.tsx
+++ src/components/ui/modal/Modal.tsx
@@ -1,13 +1,41 @@
 import { XMarkIcon } from '@heroicons/react/24/outline';
-import { Box, Container, IconButton, Modal, Typography } from '@mui/material';
+import {
+	Box,
+	Container,
+	FormControl,
+	IconButton,
+	Input,
+	Modal,
+	Typography,
+} from '@mui/material';
+import { ChangeEvent, Dispatch, KeyboardEvent, SetStateAction } from 'react';
 
 export type SearchModalPropsType = {
 	open: boolean;
 	onClose: () => void;
 	title: string;
+	search: string;
+	setSearch: Dispatch<SetStateAction<string>>;
 };
 
-const SearchModal = ({ open, onClose, title }: SearchModalPropsType) => {
+const SearchModal = ({
+	open,
+	onClose,
+	title,
+	search,
+	setSearch,
+}: SearchModalPropsType) => {
+	const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+		setSearch(e.target.value);
+	};
+
+	const onKeyUp = (e: KeyboardEvent<HTMLInputElement>) => {
+		if (e.code === 'Enter' && search.length > 0) {
+			// eslint-disable-next-line no-console
+			console.log('ok', search);
+		}
+	};
+
 	return (
 		<Modal open={open}>
 			<Box className='absolute left-1/2 top-1/2 h-screen w-full -translate-x-1/2 -translate-y-1/2 bg-primary-50'>
@@ -35,6 +63,25 @@ const SearchModal = ({ open, onClose, title }: SearchModalPropsType) => {
 						>
 							{title}
 						</Typography>
+
+						<FormControl
+							variant='standard'
+							classes={{ root: 'w-full bg-transparent mt-4' }}
+						>
+							<Input
+								type={'text'}
+								placeholder='Enter a movie'
+								value={search}
+								onChange={handleSearch}
+								onKeyUp={onKeyUp}
+								disableUnderline
+								autoFocus
+								classes={{
+									input:
+										'p-0 h-[4.5rem] text-7xl uppercase text-primary-400 placeholder:text-primary-300',
+								}}
+							/>
+						</FormControl>
 					</Box>
 				</Container>
 			</Box>
--- src/components/ui/modal/SearchMovieModal.tsx
+++ src/components/ui/modal/SearchMovieModal.tsx
@@ -1,9 +1,20 @@
 import Modal, { SearchModalPropsType } from '@components/ui/modal/Modal';
+import { useState } from 'react';
 
 type SearchMovieModal = Pick<SearchModalPropsType, 'open' | 'onClose'>;
 
 const SearchMovieModal = ({ open, onClose }: SearchMovieModal) => {
-	return <Modal open={open} onClose={onClose} title='Search movie' />;
+	const [search, setSearch] = useState<string>('');
+
+	return (
+		<Modal
+			open={open}
+			onClose={onClose}
+			title='Search movie'
+			search={search}
+			setSearch={setSearch}
+		/>
+	);
 };
 
 export default SearchMovieModal;
