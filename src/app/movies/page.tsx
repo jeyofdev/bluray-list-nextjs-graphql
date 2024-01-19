554076faa788e7278d6cf6389fc692b72e83e629
jeyofdev <jgregoire.pro@gmail.com>
2024-01-19 17:01:42 +0100

    feat : create component button-action

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -1,7 +1,9 @@
 'use client';
 
 import ContentContainer from '@components/containers/ContentContainer';
-import { Typography } from '@mui/material';
+import { ButtonAction } from '@components/ui/buttons/ButtonAction';
+import AddIcon from '@mui/icons-material/Add';
+import { Box, Typography } from '@mui/material';
 
 const MoviesPage = () => {
 	return (
@@ -9,6 +11,15 @@ const MoviesPage = () => {
 			<Typography variant='h4' component='h1' className='text-center'>
 				Movies
 			</Typography>
+
+			<Box className='mt-4 flex justify-center'>
+				<ButtonAction
+					icon={<AddIcon className='size-6' />}
+					onClick={() => {
+						// open modal
+					}}
+				/>
+			</Box>
 		</ContentContainer>
 	);
 };
