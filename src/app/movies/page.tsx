10d65a356f87e4b32d74c0b4b6404b3d9978761b
jeyofdev <jgregoire.pro@gmail.com>
2024-01-20 17:58:16 +0100

    feat : show movies list

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -1,14 +1,21 @@
 'use client';
 
+import MovieCard from '@components/cards/MovieCard';
 import ContentContainer from '@components/containers/ContentContainer';
 import { ButtonAction } from '@components/ui/buttons/ButtonAction';
+import ItemsList from '@components/ui/list/ItemList';
 import SearchMovieModal from '@components/ui/modal/SearchMovieModal';
+import { useMoviesSuspenseQuery } from '@graphql/__generated__/graphql-type';
 import AddIcon from '@mui/icons-material/Add';
 import { Box, Typography } from '@mui/material';
-import { useState } from 'react';
+import { Suspense, useState } from 'react';
 
 const MoviesPage = () => {
 	const [showSearchModal, setshowSearchModal] = useState<boolean>(false);
+
+	const { data } = useMoviesSuspenseQuery({
+		fetchPolicy: 'cache-and-network',
+	});
 
 	const handleOpenSearchModal = () => {
 		setshowSearchModal(true);
@@ -23,6 +30,20 @@ const MoviesPage = () => {
 			<Typography variant='h4' component='h1' className='text-center'>
 				Movies
 			</Typography>
+
+			<Suspense fallback={<h1>load</h1>}>
+				<ItemsList
+					items={data.movies}
+					renderItems={(movie: any) => (
+						<MovieCard
+							key={movie.id}
+							movie={movie.details}
+							supports={movie.support}
+							onClick={() => {}}
+						/>
+					)}
+				/>
+			</Suspense>
 
 			<Box className='mt-4 flex justify-center'>
 				<ButtonAction
