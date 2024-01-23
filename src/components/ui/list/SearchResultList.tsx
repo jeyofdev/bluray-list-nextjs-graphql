3116217a93bc4f1b5e4f80c5800a8c2e4999e273
jeyofdev <jgregoire.pro@gmail.com>
2024-01-23 18:41:21 +0100

    feat : remove card with no image

--- src/components/ui/list/SearchResultList.tsx
+++ src/components/ui/list/SearchResultList.tsx
@@ -11,7 +11,7 @@ const SearchResultList = ({
 }: SearchResultListPropsType) => {
 	return (
 		<Box className='grid grid-cols-1 gap-4 sm:grid-cols-4 sm:gap-4 md:grid-cols-5 lg:grid-cols-6 lg:gap-5'>
-			{items?.map((data: any) => renderItems(data))}
+			{items?.map((data: any) => (data.poster_path ? renderItems(data) : null))}
 		</Box>
 	);
 };
